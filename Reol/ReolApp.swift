7b5a784645bc490a046283ec4f389e46ef87611c
reollzhao <763142485@qq.com>
2025-11-28 19:26:20 +0800

    Initial Commit

---
+++ Reol/ReolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReolApp.swift
+//  Reol
+//
+//  Created by reol on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct ReolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
